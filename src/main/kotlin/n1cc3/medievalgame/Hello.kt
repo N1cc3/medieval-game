ed6c2409bb6a5267f7cd860085a5ce4067fa0721
Niclas Lindgren <niclas.lindgren@aalto.fi>
2018-05-03 15:53:37 +0300

    Hello world rest service.

---
+++ src/main/kotlin/n1cc3/medievalgame/Hello.kt
@@ -0,0 +1,3 @@
+package n1cc3.medievalgame
+
+data class Hello(val id: Long, val content: String)
---
+++ src/main/kotlin/n1cc3/medievalgame/RestServices.kt
@@ -0,0 +1,17 @@
+package n1cc3.medievalgame
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class RestServices {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Hello(counter.incrementAndGet(), "Hello, $name")
+
+}
